ca70addcfe310ae24039c3817b99e854698b3291
Guilherme Henrique <115844984+GuilhermeHenrii@users.noreply.github.com>
2023-12-07 21:05:42 -0300

    add funcionalidade de logout e icone de logado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserAlt, FaPowerOff } from 'react-icons/fa';
+import { RiRadioButtonLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 
 import { Nav } from './styled';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+    toast.success('Seção encerrada');
+    window.setTimeout(() => {
+      history.go(0);
+    }, 2000);
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -13,9 +31,20 @@ export default function Header() {
       <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+
+      {/* Se o usuario estiver logado, renderiza o botao de logout que redireciona para a rota de logout */}
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      {/* Ícone para informar usuario logado */}
+      {isLoggedIn && <RiRadioButtonLine size={24} color="#33cc33" />}
     </Nav>
   );
 }
